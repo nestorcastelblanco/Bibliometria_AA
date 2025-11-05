bea2f3bca94a7273f05a71b4ee7d46b4f54b68a1
sebastianagudelom <sebastianagudelomendez@gmail.com>
2025-11-05 16:23:10 -0500

    Eliminar perfiles de navegador y agregar .gitignore apropiado

--- chrome_profile_copy/Extensions/pccckmaobkjjboncdfnnofkonhgpceea/1.1.7_0/js/duckduckgoInject.js
+++
@@ -1,4 +0,0 @@
-// Injects script "duckduckgoInjected.js" into main frame
-const injectedScript = document.createElement('script');
-injectedScript.src = chrome.runtime.getURL('js/duckduckgoInjected.js');
-(document.head || document.documentElement).appendChild(injectedScript);
